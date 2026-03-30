31d0dce9ce7bad30168e695bf024f881f42ad215
roj-anbar <rojin.anbar@gmail.com>
2026-03-30 19:31:26 -0400

    Minor changes

--- utilities/tar.sh
+++ utilities/tar.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#SBATCH --partition=debug
+#SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks=192
-#SBATCH --time=00:59:59
+#SBATCH --time=00:15:59
 #SBATCH --job-name tarring
 #SBATCH --output=tarring_%j.txt
 #SBATCH --mail-type=FAIL
 
 # IMPORTANT NOTE:
 # All the untarring should happen from tarred files directly on Anna's directory!!
+# You have to run this on a compute node (don't use login/debug nodes)
+# The job might not show as completed but the files have been extracted.
 
 
 #------To untar a tarred case
@@ -22,8 +24,17 @@
 # tar --use-compress-program=pigz -tf case_043_low.tar.gz | head -50
 
 #tar --use-compress-program=pigz -xvf <tarred_file.tar.gz> <"relative/path/to/folder"> -C $PROJECT/<path_to_untar>
+
 tar --use-compress-program=pigz -xvf \
 /project/rrg-steinman-ab/ahaleyyy/DPQ/cases/tarred_cases/case_A/PTSeg028_base_0p512.tar.gz \
-"/project/rrg-steinman-ab/ahaleyyy/DPQ/cases/tarred_cases/case_A/PTSeg028_base_0p512/mesh/" \
--C $PROJECT/BSL_cases/PT_cases/AnnaH_cases/Study2_DPQ
-#echo "Completed untarring case"
+"PTSeg028_base_0p512/mesh/" \
+-C $PROJECT/BSL_cases/PT_cases/AnnaH_cases/Study2_DPQ/case_A
+
+echo "Completed untarring 1 ..."
+
+#tar -xvzf \
+#/project/rrg-steinman-ab/ahaleyyy/DPQ/cases/tarred_cases/case_A/PTSeg028_base_0p512.tar.gz \
+#"PTSeg028_base_0p512/data/"
+#-C $SCRATCH
+
+#echo "Completed untarring 2 ..."
