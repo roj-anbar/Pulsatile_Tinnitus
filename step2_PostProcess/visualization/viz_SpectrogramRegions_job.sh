886a86a50a75f3dd3a202abc6da374b56cd425d9
roj-anbar <rojin.anbar@gmail.com>
2026-07-08 15:31:26 -0400

    Implemented defining camera params through config.yaml file

--- step2_PostProcess/visualization/viz_SpectrogramRegions_job.sh
+++ step2_PostProcess/visualization/viz_SpectrogramRegions_job.sh
@@ -39,10 +39,11 @@ echo "Job started: $(date)"
 # ---------------------------------- Define Paths -----------------------------------------------------------------------
 CASE=PTSeg028_base_0p64                                                             # Case name
 BASE_DIR=$SCRATCH/My_Projects/Study1_PTRamp/cases/$CASE                             # Parent directory of the case
-MESH_STL="$BASE_DIR/step1_CFD/data/${CASE}.h5"                                      # Path to STL mesh surface file
+MESH_STL="$BASE_DIR/step1_CFD/data/${CASE}.h5"                                      # Path to HDF5 mesh surface file
 CENTERLINE_CSV="$BASE_DIR/step1_CFD/data/${CASE}_centerline_points.csv"             # Path to centerline CSV
 REGIONS_CSV="$BASE_DIR/step2_PostProcess/configs/${CASE}_spectrogram_regions.csv"  # Path to spectrogram regions CSV
-OUTPUT_PNG="$BASE_DIR/step2_PostProcess/Spectrogram_wallpressure/${CASE}_spec_regions.png"     # Path to output PNG
+OUTPUT_PNG="$BASE_DIR/step2_PostProcess/Spectrogram_wallpressure/${CASE}_spec_regions2.png"     # Path to output PNG
+CONFIG="$BASE_DIR/step2_PostProcess/configs/${CASE}_viz_config.yaml"                # Path to viz config YAML
 
 SCRIPT="/scratch/ranbar/My_Projects/Study1_PTRamp/scripts/step2_PostProcess/visualization/viz_SpectrogramRegions.py"
 
@@ -58,16 +59,19 @@ mkdir -p "$(dirname "$OUTPUT_PNG")"
 
 # --------------------------------- Run Script --------------------------------------------------------------------------
 python "$SCRIPT" \
-    --path_mesh_stl       "$MESH_STL"    \
+    --path_mesh_stl       "$MESH_STL"       \
     --path_centerline_csv "$CENTERLINE_CSV" \
     --path_regions_csv    "$REGIONS_CSV"    \
-    --path_output_png     "$OUTPUT_PNG"     
+    --path_output_png     "$OUTPUT_PNG"     \
+    --config_file         "$CONFIG"
 #    --bg_color            "white"           \
 #    --mesh_opacity        0.1               \
 #    --mesh_color          "lightgrey"       \
 #    --window_size         1800 2000         \
-#    --camera_pos          "auto"
-
+#    --cam_position        -285 -33.8 102   \
+#    --cam_focal_point     20.6 -35.1 -2.8  \
+#    --cam_view_up         0.32 0.04 0.94   \
+#    --cam_parallel_scale  10
 
 
 
@@ -79,7 +83,7 @@ python "$SCRIPT" \
 #     --path_mesh_stl       "/Users/rojin/Dropbox/My_Projects/Study1_PTRamp/cases/PTSeg028_base_0p64/step1_CFD/data/PTSeg028_base_0p64.h5" \
 #     --path_centerline_csv "/Users/rojin/Dropbox/My_Projects/Study1_PTRamp/cases/PTSeg028_base_0p64/step1_CFD/data/PTSeg028_base_0p64_centerline_points.csv" \
 #     --path_regions_csv    "/Users/rojin/Dropbox/My_Projects/Study1_PTRamp/cases/PTSeg028_base_0p64/step2_PostProcess/PTSeg028_base_0p64_spectrogram_regions_test.csv"    \
-#     --path_output_png     "/Users/rojin/Dropbox/My_Projects/Study1_PTRamp/cases/PTSeg028_base_0p64/step2_PostProcess/Visualization/PTSeg028_base_0p64_spec_regions.png"
+#     --path_output_png     "/Users/rojin/Dropbox/My_Projects/Study1_PTRamp/cases/PTSeg028_base_0p64/step2_PostProcess/Visualization/PTSeg028_base_0p64_spec_regions_top.png"
 
 
 wait
